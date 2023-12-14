85eb6004257e37d7246786b72cc270f18b7951fa
mmulyana <mulyan.t20@gmail.com>
2023-12-14 17:30:28 +0700

    update layout home

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,10 +1,6 @@
 import Footer from '@/shared/footer'
 
-type Props = {
-  children: React.ReactNode
-  detail: React.ReactNode
-}
-export default function HomeLayout({ children }: Props) {
+export default function HomeLayout({ children }: React.PropsWithChildren) {
   return (
     <main className='container mx-auto max-w-[960px] px-4'>
       {children}
